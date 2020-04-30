ac8763193574232cf68c35e4c6e9af4e35d7d79a
Jovan Mitrevski <jovan@o2map.com>
2020-04-30 12:04:34 -0500

    missed a needed changed

--- FitFID.py
+++ FitFID.py
@@ -112,8 +112,8 @@ def main():
 
     args = parser.parse_args()
 
-    x0, fwhm, phase = fitFID(args.onRes, args.offRes)
-    print(f"{x0:.4f},{fwhm:.4f},{phase:.2f}")
+    field, x0, fwhm, phase = fitFID(args.onRes, args.offRes)
+    print(f"{field},{x0:.4f},{fwhm:.4f},{phase:.2f}")
 
 if __name__ == "__main__":
     # execute only if run as a script
